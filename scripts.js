5b19d0f10d39e2a1a6e11a7be10b9892121409a0
s-jad <s_jad@web.de>
2023-07-25 12:40:38 +0200

    Feat: added login modal for current members

--- scripts.js
+++ scripts.js
@@ -4,6 +4,10 @@ const password = document.querySelector('#user-pass');
 const confirmPassword = document.querySelector('#user-pass-confirm');
 const passwordValidationDisplay = document.querySelector('#password-validation-display');
 const emailValidationDisplay = document.querySelector('#email-validation-display');
+const openLogin = document.querySelector('#open-login');
+const closeLogin = document.querySelector('#close-login');
+const modalContainer = document.querySelector('.modal-container');
+const modalContent = document.querySelector('.modal-content');
 
 createAccountBtn.addEventListener('click', function() {
     passwordValidationDisplay.innerText = "";
@@ -11,30 +15,66 @@ createAccountBtn.addEventListener('click', function() {
 
     if (password.value === confirmPassword.value) {
         if (password.validity.valid && confirmPassword.validity.valid && email.validity.valid) {
+            email.style.boxShadow = "inset 0 0 5px var(--valid-clr-60)";
+            password.style.boxShadow = "inset 0 0 5px var(--valid-clr-60)";
+            confirmPassword.style.boxShadow = "inset 0 0 5px var(--valid-clr-60)";
             passwordValidationDisplay.style.color = "green";
             passwordValidationDisplay.innerText = "Success!";
         } else {
             if (password.validity.valid && confirmPassword.validity.valid) {
+                password.style.boxShadow = "inset 0 0 5px var(--valid-clr-60)";
+                confirmPassword.style.boxShadow = "inset 0 0 5px var(--valid-clr-60)";
+
                 emailValidationDisplay.style.color = "red";
+                email.style.boxShadow = "inset 0 0 5px var(--accent-clr-60)";
                 emailValidationDisplay.innerText = "Please input a valid email";
             } else if (email.validity.valid) {
+                email.style.boxShadow = "inset 0 0 5px var(--valid-clr-60)";
+
+                password.style.boxShadow = "inset 0 0 5px var(--accent-clr-60)";
+                confirmPassword.style.boxShadow = "inset 0 0 5px var(--accent-clr-60)";
                 passwordValidationDisplay.style.color = "red";
                 passwordValidationDisplay.innerText = "Please input a valid password (8-40 characters)";
             } else {
+                email.style.boxShadow = "inset 0 0 5px var(--accent-clr-60)";
                 emailValidationDisplay.style.color = "red";
                 emailValidationDisplay.innerText = "Please input a valid email";
+
+                password.style.boxShadow = "inset 0 0 5px var(--accent-clr-60)";
+                confirmPassword.style.boxShadow = "inset 0 0 5px var(--accent-clr-60)";
                 passwordValidationDisplay.style.color = "red";
                 passwordValidationDisplay.innerText = "Please input a valid password (8-40 characters)";
             }
         }
     } else {
         if (!email.validity.valid) {
+            email.style.boxShadow = "inset 0 0 5px var(--accent-clr-60)";
             emailValidationDisplay.style.color = "red";
             emailValidationDisplay.innerText = "Please input a valid email";
         }
+        password.style.boxShadow = "inset 0 0 5px var(--accent-clr-60)";
+        confirmPassword.style.boxShadow = "inset 0 0 5px var(--accent-clr-60)";
+        password.value = "";
+        confirmPassword.value = "";
         passwordValidationDisplay.style.color = "red";
         passwordValidationDisplay.innerText = "Passwords must match!";
     }
 });
 
+openLogin.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("open-login clicked!");
+    modalContainer.style.display = "flex";
+    modalContainer.style.alignItems = "center";
+    modalContainer.style.justifyContent = "center";
+    modalContent.classList.toggle("fade");
+});
+
+
+closeLogin.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("close-login clicked!");
+    modalContainer.style.display = "none";
+    modalContent.classList.toggle("fade");
+});
 
